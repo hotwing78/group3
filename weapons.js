ae4cc69df55b9b09638be08c42f9d3c9878ff34f
geomaas <geoff.maas@gmail.com>
2016-06-14 16:55:20 -0400

    small stuff

--- weapons.js
+++ weapons.js
@@ -3,10 +3,11 @@ function Weapon(opts) {
   this.damage = opts ? opts.damage : Math.floor(Math.random() * 10);
 }
 
-var sword = new Weapon({name: "Sword", damage: Math.floor(Math.random() * 20)});
-var katana = new Weapon({name: "Katana", damage: Math.floor(Math.random() * 10)});
+var sword = new Weapon({name: "Sword", Damage: Math.floor(Math.random() * 20)});
+var katana = new Weapon({name: "Katana", Damage: Math.floor(Math.random() * 18)});
+var lance = new Weapon({name: "Lance", Damage: Math.floor(Math.random() * 25)})
+var throwingStar = new Weapon({name: "Throwing Star", Damage:Math.floor(Math.random() * 16)})
 
-
-var joker = new Villain({weapon: bomb});
-var batman = new Hero({weapon: wolfsbane});
-var superman = new Hero({name: "Superman", health: 98, weapon: laserEyes});
+// var joker = new Villain({weapon: bomb});
+// var batman = new Hero({weapon: wolfsbane});
+// var superman = new Hero({name: "Superman", health: 98, weapon: laserEyes});
