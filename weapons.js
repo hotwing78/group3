37d45852226336918d1830cf7e3dbfb2d459d9a4
geomaas <geoff.maas@gmail.com>
2016-06-14 16:11:35 -0400

    started weapons constructor

---
+++ weapons.js
@@ -0,0 +1,12 @@
+function Weapon(opts) {
+  this.name = opts ? opts.name : 'bear hands';
+  this.damage = opts ? opts.damage : Math.floor(Math.random() * 10);
+}
+
+var sword = new Weapon({name: "Sword", damage: Math.floor(Math.random() * 20)});
+var katana = new Weapon({name: "Katana", damage: Math.floor(Math.random() * 10)});
+
+
+var joker = new Villain({weapon: bomb});
+var batman = new Hero({weapon: wolfsbane});
+var superman = new Hero({name: "Superman", health: 98, weapon: laserEyes});
