f59b64dffc495064333614bed5dd50aea949530d
Robert Suber <robertsuber@Roberts-MacBook-Pro.local>
2016-06-14 16:55:26 -0400

    new changes

--- characters.js
+++ characters.js
@@ -8,70 +8,88 @@
 // KnightsNinjas.events();
 // },
 //events: function() {
-function Combatant(opts){
+function Knight(opts){
   this.Name =opts && opts.Name ? opts.Name : 'Default';
-  this.Attack= console.log("I attacked you!!");
+  this.Attack= function(enemy) {
+    console.log(`KYA!!. ${this.Name} beat ${enemy.Name} for ${this.Damage}`);
+  enemy.health -=this.Damage;
+  };
   this.Health= opts && opts.Health ? opts.Health: 100;
-  console.log(this.Name)
+  console.log(this.Name);
+  this.Damage=opts && opts.Damage ? opts.Damage: 0;
+}
+function Ninja(opts){
+  this.Name =opts && opts.Name ? opts.Name : 'Default';
+  this.Attack= function(enemy) {
+    console.log(`KYA!!. ${this.Name} beat ${enemy.Name}`)
+    return (enemy.Health =enemy.Health - this.Damage);
+     if (enemy.Health<=0){
+       console.log(`${enemy.Name} is dead!!!!`);
+     };
+    };
+  };
+  this.Health= opts && opts.Health ? opts.Health: 100;
+  console.log(this.Name);
+  this.Damage=opts && opts.Damage ? opts.Damage: 0;
 }
 var knight ={
   Name:"Knight",
-  Attack:25,
+  Damage:25,
   Health:100,
   Defence:10,
 }
 var calvary = {
   Name:"Calvary",
-  Attack:15,
+  Damage:15,
   Health:100,
   Defence:5,
 }
 var squire = {
   Name:"Squire",
-  Attack:12,
+  Damage:12,
   Health:100,
   Defence:0,
 }
 var peasant = {
   Name:"Peasant",
-  Attack:6,
+  Damage:6,
   Health:100,
   Defence:0,
 }
-var shogon = {
+var shogun = {
   Name:"Shogun",
-  Attack:25,
+  Damage:25,
   Health:100,
   Defence:0,
 
 }
 var assasin = {
   Name:"Assasin",
-  Attack:15,
+  Damage:15,
   Health:100,
   Defence:0,
 
 }
 var samurai = {
   Name:"Samurai",
-  Attack:12,
+  Damage:12,
   Health:100,
   Defence:0,
 
 }
 var monk = {
   Name:"Monk",
-  Attack:6,
+  Damage:6,
   Health:100,
   Defence:0,
 }
-var monk = new Combatant(monk);
-var samurai = new Combatant(samurai);
-var assasin= new Combatant(assasin);
-var shogun= new Combatant(shogun);
-var peasant=new Combatant(peasant);
-var squire=new Combatant(squire);
-var calvary=new Combatant(calvary);
-var knight=new Combatant(knight);
+var monk = new Ninja(monk);
+var samurai = new Ninja(samurai);
+var assasin= new Ninja(assasin);
+var shogun= new Ninja(shogun);
+var peasant=new Knight(peasant);
+var squire=new Knight(squire);
+var calvary=new Knight(calvary);
+var knight=new Knight(knight);
 // }
 // }
