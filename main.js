d72d7ea9711bab41a142f9239ac4f1d8fd764f67
Robert Suber <robertsuber@Roberts-MacBook-Pro.local>
2016-06-14 13:38:05 -0400

    roughdraft

---
+++ main.js
@@ -0,0 +1,31 @@
+var Knights = {
+  name:"Knights",
+
+}
+var Calvary = {
+  name:"Calvary",
+
+}
+var Squire = {
+  name:"Squire",
+
+}
+var Peasant = {
+  name:"Peasant",
+}
+var knights = {
+  name:"Shogun",
+
+}
+var knights = {
+  name:"Assasin",
+
+}
+var knights = {
+  name:"Samauri",
+
+}
+var knights = {
+  name:"Monk",
+
+}
