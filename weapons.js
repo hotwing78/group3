96174d38bc293aeea4ad6c67c16d3275db9c024c
Robert Suber <robertsuber@Roberts-MacBook-Pro.local>
2016-06-15 13:03:13 -0400

    changes

--- weapons.js
+++ weapons.js
@@ -5,8 +5,3 @@ function Weapon(opts) {
 
 var sword = new Weapon({name: "Sword", damage: Math.floor(Math.random() * 20)});
 var katana = new Weapon({name: "Katana", damage: Math.floor(Math.random() * 10)});
-
-
-// var joker = new Villain({weapon: bomb});
-// var batman = new Hero({weapon: wolfsbane});
-// var superman = new Hero({name: "Superman", health: 98, weapon: laserEyes});
